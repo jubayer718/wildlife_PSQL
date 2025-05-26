86c5baa3a57e76539222efd7abe8f0c33074ff58
jubayer718 <jubayerahmed2462@gmail.com>
2025-05-26 06:24:31 +0600

    Insert rangers and species table data

--- wildlife.sql
+++ wildlife.sql
@@ -5,18 +5,16 @@
 CREATE TABLE rangers(
   ranger_id SERIAL PRIMARY KEY,
   ranger_name VARCHAR(50) NOT NULL,
-  region VARCHAR(600)
+  region VARCHAR(100) NOT NULL
 )
 
-CREATE TABLE species(
-  species_id SERIAL PRIMARY KEY,
-  common_name VARCHAR(50),
-  scientific_name VARCHAR(50),
-  discovery_date VARCHAR(20),
-  conservation_status VARCHAR(500)
-)
-
-
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
 
 
  CREATE TABLE sightings (
@@ -31,5 +29,31 @@ CREATE TABLE species(
  );
 
 
+--insert rows and data to ranger table
+INSERT INTO rangers(ranger_name, region)
+ VALUES
+ ('Alice Green', 'Northern Hills'),
+ ('Bob White', 'River Delta'),
+ ('Carol', 'Mountain Range')
 
-DROP TABLE sightings;
+ SELECT * from rangers;
+
+
+--insert rows and data to species table
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status)
+VALUES 
+  ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+  ('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+  ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+  ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+-- problem--1
+
+INSERT INTO rangers(ranger_name, region)
+ VALUES
+ ( 'Derek Fox', 'Coastal Plains');
+
+--  problem--2
+
